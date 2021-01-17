ef9295be2bcff68645526c610005a574965cf6e8
JLopezz3 <lopezjerel@gmail.com>
2021-01-17 17:58:43 -0600

    chatView and chatTimer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import Preview from "./Preview";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +12,12 @@ function App() {
       <Router>
         <div>
           <Switch>
+            <Route path="/chats/view">
+              <ChatView />
+            </Route>
+            <Route path="/chats">
+              <Chats />
+            </Route>
             <Route path="/preview">
               <Preview />
             </Route>
--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -3,20 +3,28 @@ import { createSlice } from "@reduxjs/toolkit";
 export const appSlice = createSlice({
   name: "app",
   initialState: {
-    value: 0,
+    user: null,
+    selectedImage: null
   },
   reducers: {
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    login: (state, action) => {
+      state.value = action.payload;
     },
+    logout: (state) => {
+      state.user = null;
+    },
+    selectImage: (state, action) => {
+      state.selectedImage = action.payload
+    },
+    resetImage: (state) => {
+      state.selectedImage = null;
+    }
   },
 });
 
-export const { incrementByAmount } = appSlice.actions;
+export const { login, logout, selectImage, resetImage } = appSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectapp = (state) => state.app.value;
+export const selectUser = (state) => state.app.user;
+export const selectSelectedImage = (state) => state.app.selectedImage;
 
 export default appSlice.reducer;
