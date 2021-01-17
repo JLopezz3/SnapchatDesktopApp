b569e521685ca097be2265686bf315b6427dc189
JLopezz3 <lopezjerel@gmail.com>
2021-01-17 16:10:11 -0600

    setup Firestore

--- src/Preview.js
+++ src/Preview.js
@@ -5,6 +5,17 @@ import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
 import "./Preview.css";
 import CloseIcon from "@material-ui/icons/Close";
 import { useDispatch } from "react-redux";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from "@material-ui/icons/Send";
+import { v4 as uuid } from "uuid";
+import { db, storage } from "./firebase";
+import firebase from "firebase";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -18,13 +29,59 @@ function Preview() {
   }, [cameraImage, history]);
 
   const closePreview = () => {
-      dispatch(resetCameraImage());
+    dispatch(resetCameraImage());
+  };
+
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, "data_url");
+
+    uploadTask.on(
+      "state_changed",
+      null,
+      (error) => {
+        //ERROR FUNCTION
+        console.log(error);
+      },
+      () => {
+        //COMPLETE FUNCTION
+        storage
+          .ref("posts")
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              imageUrl: url,
+              username: "Jerel",
+              read: false,
+              // profilePic,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            history.replace("/chats");
+          });
+      }
+    );
   };
 
   return (
     <div className="preview">
-      <CloseIcon onClick={closePreview} className="preview__close"/>
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt="" />
+      <div onClick={sendPost} className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize="small" className="preview__sendIcon" />
+      </div>
     </div>
   );
 }
