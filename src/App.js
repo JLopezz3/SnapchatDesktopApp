019319796362a868c509ca9e563d62ed0dedae74
JLopezz3 <lopezjerel@gmail.com>
2021-01-17 19:04:07 -0600

    Create Login

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import './App.css';
-import WebcamCapture from './WebcamCapture';
+import React, { useEffect } from "react";
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
 import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { login, logout, selectUser } from "./features/appSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
-        <div>
-          <Switch>
-            <Route path="/chats/view">
-              <ChatView />
-            </Route>
-            <Route path="/chats">
-              <Chats />
-            </Route>
-            <Route path="/preview">
-              <Preview />
-            </Route>
-            <Route exact path="/">
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route path="/chats">
+                <Chats />
+              </Route>
+              <Route path="/preview">
+                <Preview />
+              </Route>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
--- src/Chats.js
+++ src/Chats.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react";
 import "./Chats.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import Chat from "./Chat";
-
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/appSlice";
+import { resetCameraImage } from "./features/cameraSlice";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import { useHistory } from "react-router-dom";
 function Chats() {
   const [posts, setPosts] = useState([]);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     db.collection("posts")
@@ -22,12 +29,21 @@ function Chats() {
       );
   }, []);
 
+  const takeSnap = () => {
+      dispatch(resetCameraImage());
+      history.push("/");
+  };
+
   return (
     <div className="chats">
       <div className="chats__header">
-        <Avatar className="chats__avatar" />
+        <Avatar 
+            src={user.profilePic} 
+            onClick={() => auth.signOut()} 
+            className="chats__avatar" 
+        />
         <div className="chats__search">
-          <SearchIcon />
+          <SearchIcon className="chats__searchIcon" />
           <input placeholder="Friends" type="text" />
         </div>
         <ChatBubbleIcon className="chats__chatIcon" />
@@ -50,6 +66,11 @@ function Chats() {
           )
         )}
       </div>
+      <RadioButtonUncheckedIcon
+        className="chats__takePicIcon"
+        onClick={takeSnap}
+        fontSize="large"
+       />
     </div>
   );
 }
